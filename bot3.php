8a7d4de74ee351e1b27e5fd6a8f8d0aa24f222bb
ball296 <57444299+ball296@users.noreply.github.com>
2020-04-07 01:41:01 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -23,7 +23,9 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => ['type' => 'text', 'text' => $text ];
+            'messages' => [
+            ['type' => 'text', 
+             'text' => json_encode($request_array)];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
