7ff4c066876edbed53b38758516f54fc2f347c49
ball296 <57444299+ball296@users.noreply.github.com>
2020-04-08 12:06:28 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -13,7 +13,7 @@ $request_array = json_decode($request, true);   // Decode JSON to Array
 
 
 
-if (sizeof($request_array['events']) > 0 && strpos($request_array['events'],'Jarvis')!==0) {
+if (sizeof($request_array['events']) > 0 && strpos($request_array['events'],'Jarvis')==0) {
 
     foreach ($request_array['events'] as $event) {
 
