da51c34e51b07e39acc2aad1cd5024bb701a6b5d
ball296 <57444299+ball296@users.noreply.github.com>
2020-04-11 17:43:10 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -21,25 +21,17 @@ if (sizeof($request_array['events']) > 0) {
         $reply_token = $event['replyToken'];
         
        $text = $event['message']['text']; 
-if (strpos($text,'Jarvis แสดงตัวอย่างรายงานเสาล้ม ผวก')!==false){
+if (strpos($text,'Jarvis pic')!==false){
 ##$text = $event['message']['text'];
         ##$text=join(', ', $event);
-    $text='เรียน ท่าน ผวก.,ผู้บริหาร
-
- อข.น.2 ขอสรุปรายงานเหตุการณ์เสาแรงสูงล้ม วันที่ 2 เม.ย. 63 เนื่องจากเกิดภัยพิบัติ พายุฤดูร้อน
- บริเวณ บ้านโค้งไผ่ ต.โค้งไผ่ อ.ขาณุวรลักษบุรี จ.กำแพงเพชร  ดังนี้
-       เหตุการณ์ เกิด เวลา 20:07 น. ความเสียหาย เสาแรงสูงระบบ 115 เควี.  ล้มจำนวน 19 ต้น และเสาแรงสูงระบบ 22 เควี  ล้มจำนวน 10 ต้น มีผู้ใช้ไฟได้รับผลกระทบทั้งหมดจำนวน  39,943 ราย ปัจจุบันมีการย้ายโหลด 115 KV. และระบบ 22kV. และตัดจ่ายไฟได้เกือบทั้งหมด เหลือผู้ใช้ไฟที่ยังจ่ายไฟไม่ได้ 25 ราย บริเวณที่เกิดเหตุไม่มีผู้ใช้ไฟรายสำคัญ เช่นโรงพยาบาล  และ ผู้ใช้ไฟรายสำคัญ 
-มูลค่าความเสียหาย ระบบ 115 เควี  ประมาณ 4 ล้านบาท  ระบบ 22 เควี ประมาณ 6 แสนบาท
-การไฟฟ้าส่วนภูมิภาค อำเภอขาณุวรลักษบุรี กำลังเร่งรัดดำเนินการแก้ไข ซ่อมแซมระบบไฟฟ้า ย้ายเสาออกจากแนวถนน ในวันที่ 3 เมษายน 2563 ทีมงาน กฟน.2 และ กฟอ.ขาณุวรลักษบุรี  จะเข้าดำเนินการแก้ไข  โดยด่วนต่อไป ครับ
-
-ขอรายงานข่าวนี้ เวลา 22:40 น. ครับ';
+    $text='https://drive.google.com/file/d/10SkA5xJgw3avY2zMBM7fU-Vb4YAIP1d6/view?usp=sharing';
         ##else{
         ##$text="it not ok";
     ##}
         $data = [
             'replyToken' => $reply_token,
-            ##'messages' => [['type' => 'image', 'originalContentUrl' => $text,'previewImageUrl'=>$text ]]
-            'messages' => [['type' => 'text', 'text' => $text ]]
+            'messages' => [['type' => 'image', 'originalContentUrl' => $text,'previewImageUrl'=>$text ]]
+            ##'messages' => [['type' => 'text', 'text' => $text ]]
       ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
