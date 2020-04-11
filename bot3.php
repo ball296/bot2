8eb6358020d6ad5d3a3f8d5d0c50530a7b73036d
ball296 <57444299+ball296@users.noreply.github.com>
2020-04-11 17:54:34 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -24,7 +24,7 @@ if (sizeof($request_array['events']) > 0) {
 if (strpos($text,'Jarvis pic')!==false){
 ##$text = $event['message']['text'];
         ##$text=join(', ', $event);
-    $text='hhttps://drive.google.com/file/d/10SkA5xJgw3avY2zMBM7fU-Vb4YAIP1d6/view';
+    $text='https://drive.google.com/file/d/10SkA5xJgw3avY2zMBM7fU-Vb4YAIP1d6/view?usp=sharing';
         ##else{
         ##$text="it not ok";
     ##}
