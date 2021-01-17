cc9deff3e34462bbe1fe02d4865e74f17877f186
ball296 <57444299+ball296@users.noreply.github.com>
2021-01-18 00:39:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,123 +12,49 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
 $jsonFlex = [
-    "type" => "flex",
-    "altText" => "Hello Flex Message",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "Purchase",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
-          ],
-          [
-            "type" => "text",
-            "text" => "฿ 100.00",
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "Rabbit Line Pay",
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "2019.02.14 21:47 (GMT+0700)",
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => "Payment complete.",
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
+    "type"=> "bubble",
+    "body"=> [
+      "type"=>"box",
+      "layout"=> "vertical",
+      "spacing"=> "sm",
+      "backgroundColor"=> "#F8EEEEFF",
+      "borderColor"=> "#DA2A14EA",
+      "contents"=> [
+        [
+          "type"=> "button",
+          "action"=> [
+            "type"=> "uri",
+            "label"=> "ข้อเสนอแนะฯ",
+            "uri"=> "https://liff.line.me/1655423177-8dMEraKE"
+        ],
+          "color"=> "#7A0CADFF",
+          "style"=> "primary"
+        ],
+        [
+          "type"=> "button",
+          "action"=> [
+            "type"=> "uri",
+            "label"=> "ข้อร้องเรียน",
+            "uri"=> "https://liff.line.me/1655423177-nKP1zdX1"
+        ],
+          "color"=> "#7A0CADFF",
+          "style"=> "primary"
           ]
-        ]
-      ],
-      "body" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "separator",
-            "color" => "#C3C3C3"
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "Merchant",
-                "align" => "start",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "BTS 01",
-                "align" => "end",
-                "color" => "#000000"
-              ]
-            ]
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "New balance",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "฿ 45.57",
-                "align" => "end"
-              ]
-            ]
-          ],
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
-          ]
-        ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "View Details",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://liff.line.me/1655423177-8dMEraKE"
-            ]
-          ]
-        ]
       ]
-    ]
-  ];
+    ],
+    "footer"=> [
+      "type"=> "box",
+      "layout"=> "vertical",
+      "spacing"=> "sm",
+      "contents"=> [
+        [
+          "type"=> "text",
+          "text"=> "แผนกลูกค้าสัมพันธ์",
+          "contents"=> []
+          ]
+      ]
+      ]
+];
 
 if (sizeof($request_array['events']) > 0) {
 
@@ -138,7 +64,7 @@ if (sizeof($request_array['events']) > 0) {
         $reply_token = $event['replyToken'];
         
        $text = $event['message']['text']; 
-if (strpos($text,'Jarvis flex')!==false){
+if (strpos($text,'v list')!==false){
 ##$text = $event['message']['text'];
         ##$text=join(', ', $event);
         $data = [
